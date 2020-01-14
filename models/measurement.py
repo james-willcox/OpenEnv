2e45f747987baf86765647328e06880a95d9c905
James Willcox <james.willcox@rotavi.com>
2020-01-14 16:30:22 +1100

    Models and measurements
    Started the models sub folder, and started the measurements class

---
+++ models/measurement.py
@@ -0,0 +1,15 @@
+from db import db
+from datetime import datetime
+
+class MeasurementModel(db.Model):
+    __tablename__ = "measurement"
+    __table_args__ = {"schema": "openenv"}
+
+    measurement_id = db.Column(db.Integer, primary_key=True)
+    measurement_value = db.Column(db.Float(precision=12))
+    direction = db.Column(db.Float(precision=12))
+    measurement_type_id = db.Column(db.Integer)
+    latitude = db.Column(db.Float(precision=6))
+    longitude = db.Column(db.Float(precision=6))
+    measurement_datetime = db.Column(db.DateTime)
+    station_id = db.Column(db.Integer)
