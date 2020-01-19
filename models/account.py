ff5540f011ff053e4d0edddef6b7004e0b9548a5
James Willcox <james.willcox@rotavi.com>
2020-01-19 16:28:02 +1100

    Account and Login details model
    Added in the models for the login details and the user's account.

---
+++ models/account.py
@@ -0,0 +1,22 @@
+from db import db
+from datetime import datetime
+
+
+class AccountModel(db.Model):
+    account_id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    sign_up_date = db.Column(db.DateTime, default=datetime.now())
+
+    @classmethod
+    def find_by_email(cls, email_address: str):
+        return cls.query.filter_by(email = email_address).first()
+
+    @classmethod
+    def find_by_id(cls, user_id: int):
+        return cls.query.filter_by(account_id=user_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
