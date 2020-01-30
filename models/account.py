bb810b60247873844a5b39b455605858a38e7e17
James Willcox <james.willcox@rotavi.com>
2020-01-30 15:32:25 +1100

    Changed the login system
    - Deleted the extra table for login details as it was cumbersome
    - Changed the Auth and Password hashing to argon2 with a salt to increase difficulty should anyone attempt to attack the passwords

--- models/account.py
+++ models/account.py
@@ -11,6 +11,8 @@ class AccountModel(db.Model):
     last_name = db.Column(db.String(255))
     email = db.Column(db.String(255))
     sign_up_date = db.Column(db.DateTime, default=datetime.now())
+    account_password = db.Column(db.String(256))
+    modified_timestamp = db.Column(db.DateTime, default=datetime.now())
 
     @classmethod
     def find_by_email(cls, email_address: str):
--- schemas/login_details.py
+++
@@ -1,6 +0,0 @@
-from ma import ma
-from models.login_details import LoginDetailsModel
-
-class LoginDetailsSchema(ma.ModelSchema):
-    class Meta:
-        model = LoginDetailsModel
