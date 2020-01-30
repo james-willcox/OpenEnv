ade2f5214f98ed34caa8b3ea4f3f441f1df08019
James Willcox <james.willcox@rotavi.com>
2020-01-31 10:34:22 +1100

    API user ket create
    - made some changes so that a user and a station's keys are different

--- models/api_key.py
+++ models/api_key.py
@@ -1,24 +1,31 @@
 from db import db
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class ApiKeyModel(db.Model):
     __tablename__ = "api_key"
-    __table_args__ = {"schema": "opengeo"}
+    __table_args__ = {"schema": "openenv"}
     api_key_id = db.Column(db.Integer, primary_key=True)
     account_id = db.Column(db.Integer)
-    api_key = db.Column(db.UUID(as_uuid=True), unique=True)
+    api_key = db.Column(UUID(as_uuid=True))
+    api_key_created = db.Column(db.DateTime, default=datetime.now())
+    station_id = db.Column(db.Integer)
+    key_type_id = db.Column(db.Integer)
 
-    @classmethod
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def find_by_email(cls, email_address: str):
-        return cls.query.filter_by(email=email_address).first()
+    def find_by_account_id(cls, account_id: int):
+        return cls.query.filter_by(account_id=account_id).all()
 
     @classmethod
     def find_by_api_key(cls, api_key):
-        return cls.query.filter_by(api_key=api_key)
+        return cls.query.filter_by(api_key=api_key).all()
+
+    @classmethod
+    def find_user_api_keys(cls, account_id):
+        return cls.query.filter_by(account_id=account_id, station_id=None).first()
 
--- resources/api_key.py
+++ resources/api_key.py
@@ -13,20 +13,23 @@ from schemas.api_key import ApiSchema
 api_key_schema = ApiSchema()
 
 
-class CreateApiKey(Resource):
+class CreateUserApiKey(Resource):
     @jwt_required
     def post(self):
-        request_json = request.get_json()
         account_id = get_jwt_identity()
+        if ApiKeyModel.find_user_api_keys(account_id):
+            return {'message': 'User already has an API key'}, 418
+
         new_api_key = uuid4()
         api_dict = {}
         api_dict['account_id'] = account_id
         api_dict['api_key'] = new_api_key
+        api_dict['key_type_id'] = 1
 
         try:
-            api_key = api_key_schema.load(api_dict)
-            api_key.save_to_db()
-            return {"message": f'Your API Key is: {new_api_key}'}
+            api = api_key_schema.load(api_dict)
+            api.save_to_db()
+            return {"message": f'Your API Key is: {new_api_key}'}, 201
         except:
             return {"message": f'Error creating API key'}
 
